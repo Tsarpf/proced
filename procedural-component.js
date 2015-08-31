5f87a8ad2190a54f0f0ec773c805ddb67ec00a0a
Teemu Sarapisto <tsarpf@gmail.com>
2015-08-31 03:24:23 +0300

    64 was too much... let's try 27...

--- procedural-component.js
+++ procedural-component.js
@@ -6,7 +6,7 @@ pc.script.create('procedural', function (app) {
 
     ProceduralObject.prototype = {
         initialize: function () {
-            width = height = depth = 4;
+            width = height = depth = 3;
             isolevel = 0.5;
             var vertexFormat = new pc.VertexFormat(app.graphicsDevice, [{
                     semantic: pc.SEMANTIC_POSITION,
