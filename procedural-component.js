77f1963356f77957c46e07e3acc6e544a2680ac9
Teemu Sarapisto <tsarpf@gmail.com>
2015-09-29 16:17:12 +0300

    Fix procedural-component style according to new .eslintrc

--- procedural-component.js
+++ procedural-component.js
@@ -1,13 +1,16 @@
+//globals for eslint
+/*global noise:false, PROCED:false*/
+
 var width, height, depth, isolevel, dataStep;
 pc.script.create('procedural', function (app) {
-    var ProceduralObject = function (entity) {
-        this.entity = entity;
-    }
+	var ProceduralObject = function (entity) {
+		this.entity = entity;
+	};
 
-    ProceduralObject.prototype = {
-        initialize: function () {
-            width = height = depth = 64;
-            isolevel = 0.5;
+	ProceduralObject.prototype = {
+		initialize: function () {
+			width = height = depth = 64;
+			isolevel = 0.5;
 			dataStep = {
 				x: 1 / width,
 				y: 1 / height,
@@ -15,83 +18,78 @@ pc.script.create('procedural', function (app) {
 			};
 			//noise.seed(1);
 			noise.seed(Math.random());
-            var vertexFormat = new pc.VertexFormat(app.graphicsDevice, [{
-                    semantic: pc.SEMANTIC_POSITION,
-                    components: 3,
-                    type: pc.ELEMENTTYPE_FLOAT32
-                },
-				{
-                    semantic: pc.SEMANTIC_NORMAL,
-                    components: 3,
-                    type: pc.ELEMENTTYPE_FLOAT32
-                }
-            ]);
+			var vertexFormat = new pc.VertexFormat(app.graphicsDevice, [{
+				semantic: pc.SEMANTIC_POSITION,
+				components: 3,
+				type: pc.ELEMENTTYPE_FLOAT32
+			},
+			{
+				semantic: pc.SEMANTIC_NORMAL,
+				components: 3,
+				type: pc.ELEMENTTYPE_FLOAT32
+			}
+			]);
 
-            //Marching etc done at this point
-            var vertexArray = getVertices();
+			//Marching etc done at this point
+			var vertexArray = getVertices();
 
-            var vertexBuffer = new pc.VertexBuffer(
-                app.graphicsDevice,
-                vertexFormat,
-                vertexArray.length / 2,
-                pc.BUFFER_STATIC
-            );
+			var vertexBuffer = new pc.VertexBuffer(
+					app.graphicsDevice,
+					vertexFormat,
+					vertexArray.length / 2,
+					pc.BUFFER_STATIC
+					);
 
-            var vertices = new Float32Array(vertexBuffer.lock());
+			var vertices = new Float32Array(vertexBuffer.lock());
 
-            vertices.set(vertexArray);
+			vertices.set(vertexArray);
 
-            console.log(vertexArray.length);
-            vertexBuffer.unlock();
+			//console.log(vertexArray.length);
+			vertexBuffer.unlock();
 
-            var mesh = new pc.Mesh();
-            mesh.vertexBuffer = vertexBuffer;
-            mesh.primitive[0].type = pc.PRIMITIVE_TRIANGLES;
+			var mesh = new pc.Mesh();
+			mesh.vertexBuffer = vertexBuffer;
+			mesh.primitive[0].type = pc.PRIMITIVE_TRIANGLES;
 
-            //what's this I don't even?
-            mesh.primitive[0].base = 0;
+			//what's this I don't even?
+			mesh.primitive[0].base = 0;
 
-            mesh.primitive[0].count = vertexArray.length / 6;
+			mesh.primitive[0].count = vertexArray.length / 6;
 
-            //Probably leaving this undefined would suffice. But since I couldn't find any docs about this right now let's just be safe
-            mesh.primitive[0].indexed = false;
+			//Probably leaving this undefined would suffice. But since I couldn't find any docs about this right now let's just be safe
+			mesh.primitive[0].indexed = false;
 
-            var node = new pc.GraphNode();
-            var material = new pc.PhongMaterial();
-            //var material = new pc.BasicMaterial();
-            //material.cull = 0;
-            //material.vertexColors = true;
-            var meshInstance = new pc.MeshInstance(node, mesh, material);
+			var node = new pc.GraphNode();
+			var material = new pc.PhongMaterial();
+			//var material = new pc.BasicMaterial();
+			//material.cull = 0;
+			//material.vertexColors = true;
+			var meshInstance = new pc.MeshInstance(node, mesh, material);
 
-            var model = new pc.Model();
-            model.graph = node;
-            model.meshInstances = [meshInstance];
+			var model = new pc.Model();
+			model.graph = node;
+			model.meshInstances = [meshInstance];
 
-            /*
-             * These don't work without an index buffer :(
-            model.generateWireframe();
-            model.meshInstances[0].renderStyle = pc.RENDERSTYLE_WIREFRAME;
-            */
+			app.systems.model.addComponent(this.entity, {
+				type: 'asset'
+			});
+			this.entity.model.model = model;
 
-            app.systems.model.addComponent(this.entity, {
-                type: 'asset'
-            });
-            this.entity.model.model = model;
-
-            app.systems.rigidbody.addComponent(this.entity, {
-                type: 'static'
-            });
-            app.systems.collision.addComponent(this.entity, {
-                type: 'mesh'
-            });
-            this.entity.collision.model = model;
-            app.systems.collision.implementations.mesh.doRecreatePhysicalShape(this.entity.collision);
-        },
-        update: function() {
-        }
-    }
-    return ProceduralObject;
+			app.systems.rigidbody.addComponent(this.entity, {
+				type: 'static'
+			});
+			app.systems.collision.addComponent(this.entity, {
+				type: 'mesh'
+			});
+			this.entity.collision.model = model;
+			app.systems.collision.implementations.mesh.doRecreatePhysicalShape(this.entity.collision);
+		},
+		update: function() {
+		}
+	};
+	return ProceduralObject;
 });
+/*
 function createSphere() {
 	return getVals(getSphereVal);
 }
@@ -112,60 +110,57 @@ function getSlopeVal(x, y, z) {
 function getFlatVal(x, y, z) {
 	return y / height;
 }
+*/
 function getNoiseVal(x, y, z) {
-	var octave1 = noise.simplex3(
-		x / 0.1 + dataStep.x,
-		y / 0.1 + dataStep.y,
-		z / 0.1 + dataStep.z
-	) / 10;
-	var octave2 = noise.simplex3(
-		x / 35 + dataStep.x,
-		y / 35 + dataStep.y,
-		z / 35 + dataStep.z
-	);
 	/*
+	var octave1 = noise.simplex3(
+			x / 0.1 + dataStep.x,
+			y / 0.1 + dataStep.y,
+			z / 0.1 + dataStep.z
+			) / 10;
+	var octave2 = noise.simplex3(
+			x / 35 + dataStep.x,
+			y / 35 + dataStep.y,
+			z / 35 + dataStep.z
+			);
 	var octave3 = noise.simplex3(
-		x / 50 + dataStep.x,
-		y / 50 + dataStep.y,
-		z / 50 + dataStep.z
-	);
-	*/
+			x / 50 + dataStep.x,
+			y / 50 + dataStep.y,
+			z / 50 + dataStep.z
+			);
+	   */
 	var octave3 = noise.simplex3(
-		x / 25+ dataStep.x,
-		y / 25+ dataStep.y,
-		z / 25+ dataStep.z
-	);
+			x / 25+ dataStep.x,
+			y / 25+ dataStep.y,
+			z / 25+ dataStep.z
+			);
 
 	//return octave1 + octave2 + octave3;
 	return octave3;
 }
+/*
 function getSphereVal(x, y, z) {
-    var maxDistance = width / 2;
-    var result, idx, pos;
-    var data = [];
+	var maxDistance = width / 2;
+	//var result, idx, pos;
+	var pos;
+	//var data = [];
 
-    var center = {
-        x: width / 2,
-        y: height / 2,
-        z: depth / 2
-    }
-    pos = {
-        x: x, y: y, z: z
-    } 
+	var center = {
+		x: width / 2,
+		y: height / 2,
+		z: depth / 2
+	};
+	pos = {
+		x: x, y: y, z: z
+	};
 	return 1 - (getDistance(pos, center) / maxDistance); 
 }
-
-function getNormalForVertex(x, y, z, sampler, outObj) {
-/*
-	outObj.x = sampler(x + dataStep.x, y, z) - sampler(x - dataStep.x, y , z);
-	outObj.y = sampler(x, y + dataStep.y, z) - sampler(x, y - dataStep.y , z);
-	outObj.z = sampler(x, y, z + dataStep.z) - sampler(x, y , z - dataStep.z);
 */
 
+function getNormalForVertex(x, y, z, sampler, outObj) {
 	outObj.x = -(sampler(x + dataStep.x, y, z) - sampler(x - dataStep.x, y , z));
 	outObj.y = -(sampler(x, y + dataStep.y, z) - sampler(x, y - dataStep.y , z));
 	outObj.z = -(sampler(x, y, z + dataStep.z) - sampler(x, y , z - dataStep.z));
-
 }
 
 var zeroVec = {x: 0, y: 0, z: 0};
@@ -187,7 +182,7 @@ function getVertices() {
 	var sampler = getNoiseVal;
 	//var sampler = getSphereVal;
 	//var sampler = getSlopeVal;
-    var vertices = [];
+	var vertices = [];
 	var normal = {
 		x: 0,
 		y: 0,
@@ -195,14 +190,14 @@ function getVertices() {
 	};
 
 	var xPos, yPos, zPos;
-    //subtract 1 from each end since the last one doesn't need its own cube yaknaw :S
-    for(var z = 0; z < depth - 1; z++) {
-        for(var y = 0; y < height - 1; y++) {
-            for(var x = 0; x < width - 1; x++) {
-                var cube = getCubeAtPos(x,y,z, sampler);
-                var cubeTris = [];
-                var ntriangles = PROCED.polygonize(cube, isolevel, cubeTris);
-                var count = 0;
+	//subtract 1 from each end since the last one doesn't need its own cube yaknaw :S
+	for(var z = 0; z < depth - 1; z++) {
+		for(var y = 0; y < height - 1; y++) {
+			for(var x = 0; x < width - 1; x++) {
+				var cube = getCubeAtPos(x,y,z, sampler);
+				var cubeTris = [];
+				//var ntriangles = PROCED.polygonize(cube, isolevel, cubeTris);
+				PROCED.polygonize(cube, isolevel, cubeTris);
 				for(var i = 0; i < cubeTris.length; i+=3) {
 					xPos = cubeTris[i];
 					yPos = cubeTris[i+1];
@@ -221,106 +216,92 @@ function getVertices() {
 					vertices.push(normal.z);
 
 				}
-            }
-        }
-    }
-    return vertices;
-}
-
-function getCubeAtPos(x, y, z, sampler) {
-    var cube = [];
-    cube[0] = {
-        pos: {
-            x: x,
-            y: y,
-            z: z
-        },
-        val: sampler(x,y,z)
-    }
-    cube[1] = {
-        pos: {
-            x: x + 1,
-            y: y,
-            z: z
-        },
-        val: sampler(x + 1,y,z)
-    }
-    cube[2] = {
-        pos: {
-            x: x + 1,
-            y: y,
-            z: z + 1
-        },
-        val: sampler(x + 1,y,z + 1)
-    }
-    cube[3] = {
-        pos: {
-            x: x,
-            y: y,
-            z: z + 1
-        },
-        val: sampler(x,y,z + 1)
-    }
-    cube[4] = {
-        pos: {
-            x: x,
-            y: y + 1,
-            z: z
-        },
-        val: sampler(x,y + 1, z)
-    }
-    cube[5] = {
-        pos: {
-            x: x + 1,
-            y: y + 1,
-            z: z
-        },
-        val: sampler(x + 1,y + 1,z)
-    }
-    cube[6] = {
-        pos: {
-            x: x + 1,
-            y: y + 1,
-            z: z + 1
-        },
-        val: sampler(x + 1, y + 1, z + 1)
-    }
-    cube[7] = {
-        pos: {
-            x: x,
-            y: y + 1,
-            z: z + 1
-        },
-        val: sampler(x,y + 1, z + 1)
-    }
-
-    return cube;
-}
-
-function getDistance(p1, p2) {
-    return Math.sqrt(
-        Math.pow(p1.x - p2.x, 2) + 
-        Math.pow(p1.y - p2.y, 2) +
-        Math.pow(p1.z - p2.z, 2)
-    );
-}
-
-function debugPrint(array) {
-	/*	
-	var pos = [];
-    for(var z = 0; z < depth - 1; z++) {
-        for(var y = 0; y < height - 1; y++) {
-            for(var x = 0; x < width - 1; x++) {
-			 	pos.push(getIdx(x,y,z));
-				if(pos.length === 3) {
-					console.log(pos);
-					pos = [];
-				}
 			}
 		}
 	}
-	*/
+	return vertices;
+}
 
+function getCubeAtPos(x, y, z, sampler) {
+	var cube = [];
+	cube[0] = {
+		pos: {
+			x: x,
+			y: y,
+			z: z
+		},
+		val: sampler(x,y,z)
+	};
+	cube[1] = {
+		pos: {
+			x: x + 1,
+			y: y,
+			z: z
+		},
+		val: sampler(x + 1,y,z)
+	};
+	cube[2] = {
+		pos: {
+			x: x + 1,
+			y: y,
+			z: z + 1
+		},
+		val: sampler(x + 1,y,z + 1)
+	};
+	cube[3] = {
+		pos: {
+			x: x,
+			y: y,
+			z: z + 1
+		},
+		val: sampler(x,y,z + 1)
+	};
+	cube[4] = {
+		pos: {
+			x: x,
+			y: y + 1,
+			z: z
+		},
+		val: sampler(x,y + 1, z)
+	};
+	cube[5] = {
+		pos: {
+			x: x + 1,
+			y: y + 1,
+			z: z
+		},
+		val: sampler(x + 1,y + 1,z)
+	};
+	cube[6] = {
+		pos: {
+			x: x + 1,
+			y: y + 1,
+			z: z + 1
+		},
+		val: sampler(x + 1, y + 1, z + 1)
+	};
+	cube[7] = {
+		pos: {
+			x: x,
+			y: y + 1,
+			z: z + 1
+		},
+		val: sampler(x,y + 1, z + 1)
+	};
+
+	return cube;
+}
+
+function getDistance(p1, p2) {
+	return Math.sqrt(
+			Math.pow(p1.x - p2.x, 2) + 
+			Math.pow(p1.y - p2.y, 2) +
+			Math.pow(p1.z - p2.z, 2)
+			);
+}
+
+/*
+function debugPrint(array) {
 	var pos = [];
 	for(var i = 0; i < array.length; i++) {
 		pos.push(array[i]);
@@ -331,7 +312,8 @@ function debugPrint(array) {
 
 	}
 }
-
 function getIdx(x, y, z) {
-    return x + width * (y + height * z);
+	return x + width * (y + height * z);
 }
+*/
+
