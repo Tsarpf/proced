b5ac36f90551bf64d1d8f3c75668007b177ba043
Teemu Sarapisto <tsarpf@gmail.com>
2015-10-07 17:21:02 +0300

    Add UI component

---
+++ text.js
@@ -0,0 +1,41 @@
+pc.script.create('ui', function () {
+	var UI = function (entity) {
+		this.entity = entity;
+	};
+
+	UI.prototype = {
+		initialize: function () {
+			// Create a div centred inside the main canvas
+			var div = document.createElement('div');
+			div.style.position = 'absolute';
+			div.style.width = '500px';
+			div.style.top = '50%';
+			div.style.left = '50%';
+			div.style.marginLeft = '-250px';            
+			div.style.textAlign = 'center';
+			div.style.color = 'white';
+			div.style.fontSize = 'xx-large';
+			div.style.visibility = 'hidden';
+
+			// Add the div to the DOM as a child of the document's body element
+			document.body.appendChild(div);
+
+			this.div = div;
+
+			// Set some default state on the UI element
+			this.setText('GAME OVER');
+			this.setVisibility(true);
+		},
+
+		// Some utility functions that can be called from other game scripts
+		setVisibility: function (visible) {
+			this.div.style.visibility = visible ? 'visible' : 'hidden';
+		},
+
+		setText: function (message) {
+			this.div.innerHTML = message;
+		}
+	};
+
+	return UI;
+});
