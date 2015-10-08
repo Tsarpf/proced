ff877b2eb1dbf477d48d19c76336c561834f8c45
Teemu Sarapisto <tsarpf@gmail.com>
2015-10-08 14:30:48 +0300

    Progressbar working

--- text.js
+++ text.js
@@ -59,45 +59,65 @@ pc.script.create('ui', function () {
 			this.queueMultipleText(texts);
 		},
 
+		update: function() {
+			//oispa beer
+			if(this.showBar) {
+				var total = this.endTime - this.startTime;
+				var current = performance.now();
+				if(current > this.endTime) {
+					this.showBar = false;
+				}
+				var progress  = (current - this.startTime) / total * 100;
+				this.setProgress(progress);
+			}
+		},
+
+		setProgress: function(value) {
+			this.progressBar.setAttribute('value', '' + value);
+		},
 
 		// Some utility functions that can be called from other game scripts
 		setVisibility: function (visible) {
 			this.div.style.visibility = visible ? 'visible' : 'hidden';
 		},
 
-		setText: function (message) {
-			this.div.innerHTML = message;
+		startTime: 0,
+		endTime: 0,
+		showBar: false,
+		showProgressBar: function(time) {
+			this.startTime = performance.now();
+			this.endTime = this.startTime + time;
+			this.showBar = true;
 		},
 
+		setText: function (message, time) {
+			if(time) {
+				this.showProgressBar(time);
+			}
+			this.div.innerHTML = message;
+		},
 		queueMultipleText: function(texts) {
 			var startTime = 0;
 			var that = this;
-			that.setText(texts[0].text);
+			that.setText(texts[0].text, texts[0].time);
 			for(var i = 1; i < texts.length; i++) {
 				var time = texts[i-1].time;
 				var text = texts[i].text;
 
-				var timeClosure = function(text) {
+				var timeClosure = function(text, time) {
 					return function() {
-						that.setText(text);
+						that.setText(text, time);
 					};
 				};
 
-				setTimeout(timeClosure(text), time + startTime);
+				setTimeout(timeClosure(text, texts[i].time), time + startTime);
 				startTime += time;
 			}
-		},
-
-		showTextFor: function (text, time, callback) {
-			this.setText(text);
-			setTimeout(function() {
-				callback();
-			}, time);
 		},
 		initializeProgressBar: function() {
 			var progress = document.createElement('progress');
 			this.progressBar = progress;
-			this.progressBar.setAttribute('value', '22');
+			this.setProgress(22);
 			this.progressBar.setAttribute('max', '100');
 			this.progressBar.style.position = 'absolute';
 			this.progressBar.style.width = '500px';
