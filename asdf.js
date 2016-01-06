3787ae2a85b40c038f7c4e22971483ee685487db
Teemu Sarapisto <tsarpf@gmail.com>
2016-01-06 22:01:45 +0200

    stuffs

---
+++ asdf.js
@@ -0,0 +1,5 @@
+var PROCED = window.PROCED||{};
+PROCED['surface-nets'] = require('surface-nets');
+PROCED.ndarray = require('ndarray');
+PROCED['ndarray-fill'] = require('ndarray-fill');
+window.PROCED = PROCED;
--- surface-nets.js
+++ surface-nets.js
@@ -1,7 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-require('surface-nets');
-require('ndarray');
-require('ndarray-fill');
+var PROCED = window.PROCED||{};
+PROCED['surface-nets'] = require('surface-nets');
+PROCED.ndarray = require('ndarray');
+PROCED['ndarray-fill'] = require('ndarray-fill');
+window.PROCED = PROCED;
 
 },{"ndarray":14,"ndarray-fill":13,"surface-nets":17}],2:[function(require,module,exports){
 /**
